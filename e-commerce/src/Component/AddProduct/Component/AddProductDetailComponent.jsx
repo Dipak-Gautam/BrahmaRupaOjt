a1562acfaa9eabb96ec070dff153c89e0597ee1f
Dipak-Gautam <anjangautam095@gmail.com>
2025-08-05 13:55:43 +0545

    feat: Add product created

--- e-commerce/src/Component/AddProduct/Component/AddProductDetailComponent.jsx
+++ e-commerce/src/Component/AddProduct/Component/AddProductDetailComponent.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
 import ProductInputField from "../../InputFields/ProductInputField";
-import OrangeButton from "../../Button/OrangeButton";
+import AddFeature from "./AddFeature";
+import AddCategory from "./AddCategory";
 
 const AddProductDetailComponent = ({ setProductDetail, productDetail }) => {
-  const [features, setFeatures] = useState("");
-
-  const handleAddFeature = () => {
-    if (!features.trim()) return;
-
-    setProductDetail({
-      ...productDetail,
-      features: [...productDetail.features, features.trim()],
-    });
-    setFeatures(""); // clear input after adding
-  };
-
   return (
     <div>
       <ProductInputField
@@ -24,25 +13,11 @@ const AddProductDetailComponent = ({ setProductDetail, productDetail }) => {
         productDetail={productDetail}
       />
 
-      <div className="my-2 space-y-1">
-        <div className="text-sm font-medium text-gray-700">Features:</div>
+      <AddFeature
+        productDetail={productDetail}
+        setProductDetail={setProductDetail}
+      />
 
-        <div className="text-sm">
-          Data:{" "}
-          {productDetail.features.map((item, index) => (
-            <span key={index}>{item}, </span>
-          ))}
-        </div>
-
-        <input
-          type="text"
-          value={features}
-          onChange={(e) => setFeatures(e.target.value)}
-          className="border outline-none rounded-md p-1 w-full"
-        />
-
-        <OrangeButton title="Add +" onClick={handleAddFeature} />
-      </div>
       <ProductInputField
         setProductDetail={setProductDetail}
         label={"Rating"}
@@ -55,10 +30,8 @@ const AddProductDetailComponent = ({ setProductDetail, productDetail }) => {
         name={"price"}
         productDetail={productDetail}
       />
-      <ProductInputField
+      <AddCategory
         setProductDetail={setProductDetail}
-        label={"Category"}
-        name={"category"}
         productDetail={productDetail}
       />
       <ProductInputField
