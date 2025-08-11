6a2d5dd52619ba1134bb4bf826f1742a2bce77ec
Dipak-Gautam <anjangautam095@gmail.com>
2025-08-11 21:33:01 +0545

    feat : dashboard nav created

--- e-commerce/src/Component/NavBar/NavBar.jsx
+++ e-commerce/src/Component/NavBar/NavBar.jsx
@@ -3,9 +3,11 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import { IoSearch } from "react-icons/io5";
 import { NavLink, useNavigate } from "react-router";
 import { MdOutlineSettings } from "react-icons/md";
+import { MdDashboard } from "react-icons/md";
 
 const NavBar = () => {
   const navigate = useNavigate();
+  const userData = JSON.parse(localStorage.getItem("userDetail"));
 
   return (
     <div className="flex justify-between items-center   rounded-md p-1 ">
@@ -15,14 +17,20 @@ const NavBar = () => {
         </NavLink>
       </div>
       <div className="flex items-center gap-3 md:gap-5">
-        <div className="flex border-2 border-gray-200 items-center rounded-sm  ">
+        {/* <div className="flex border-2 border-gray-200 items-center rounded-sm  ">
           <input
             type="text"
             className="outline-none p-1 text-sm w-24 md:w-52 md:text-lg "
             placeholder="search"
           />
           <IoSearch className="mr-2 text-lg" />
-        </div>
+        </div> */}
+        {userData.role == "admin" && (
+          <NavLink to="/dashboard">
+            <MdDashboard className="text-3xl text-gray-600" />
+          </NavLink>
+        )}
+
         <div>
           <NavLink to="/cart">
             <MdOutlineShoppingCart className="text-3xl text-gray-600" />
