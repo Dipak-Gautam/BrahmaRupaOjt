d38909a42dfec6506f37c06391934ea73695d012
Dipak-Gautam <anjangautam095@gmail.com>
2025-08-14 14:26:03 +0545

    code cleanup

--- e-commerce/src/Component/CoustomFunction/cancelOrderData.js
+++ e-commerce/src/Component/CoustomFunction/cancelOrderData.js
@@ -1,6 +1,5 @@
 const cancelOrderData = (data, setOrderData, orderData) => {
   let tempData = orderData.filter((item) => item._id != data);
-  console.log("tempdata", tempData);
   setOrderData(tempData);
 };
 export default cancelOrderData;
